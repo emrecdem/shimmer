fe257c23af80f9b2a08d87a0eaf2a7b463bf6660
vincentvanhees <vincentvanhees@gmail.com>
2019-02-27 11:16:46 +0100

    readme file added, example fileheader added, code tidied up to only focus on one accelerometer

--- detecthandclap.R
+++ detecthandclap.R
@@ -7,6 +7,7 @@ graphics.off()
 path = "/media/sf_sharedfolder/Emotion/accelerometer_data"
 # assumption is that there is a 'myresults' folder in this folder
 # in which the extracted features are stored.
+do.plot = TRUE # Create plot on screen (for testing). change to FALSE to turn off / change to TRUE to turn on
 #==============================================
 
 outputfolder = paste0(path,"/myresults")
@@ -15,46 +16,55 @@ if (dir.exists(handclapfolder) == FALSE) dir.create(handclapfolder)
 # extract relevant information from filenames in the 'myresults' folder
 fnamesfull = dir(outputfolder,full.names = TRUE)
 fnames = dir(outputfolder)
-getloc = function(x) return(unlist(strsplit(x,"_"))[4])
-getid = function(x) return(unlist(strsplit(x,"_"))[6])
-getses = function(x) return(unlist(strsplit(x,"_"))[5])
+getid = function(x) return(unlist(strsplit(x,"_"))[4])
+getloc = function(x) return(unlist(strsplit(x,"_"))[2])
+getses = function(x) return(unlist(strsplit(x,"_"))[3])
+getpart = function(x) return(unlist(strsplit(x,"_"))[7])
 id = as.character(sapply(X = fnames,FUN=getid))
 loc = as.character(sapply(X = fnames,FUN=getloc)) #loc = location
 ses = as.character(sapply(X = fnames,FUN=getses)) #ses = session
+part = as.character(sapply(X = fnames,FUN=getpart)) #part = partA or PartB
 wrist = loc %in% c("nondomwrist","domwrist")
 uid = unique(id)
 for (idi in uid) { # loop through individuals
-  uses = unique(ses[which(id == idi)])  
+  uses = unique(ses[which(id == idi)])
   for (sesi in uses) { # loop over sessions
-    twowrists = which(ses == sesi & id ==idi & wrist == TRUE)
-    if (length(twowrists) > 1) { # if a pair is found load the data
-      loc1 = loc[twowrists[1]]
-      if (loc1 == "nondomwrist") {
-        nondomwrist = read.csv(fnamesfull[twowrists[1]])
-        domwrist = read.csv(fnamesfull[twowrists[2]])
-      } else {
-        nondomwrist = read.csv(fnamesfull[twowrists[2]])
-        domwrist = read.csv(fnamesfull[twowrists[1]])
+    upart = unique(part[which(id == idi & ses == sesi)])  
+    for (parti in upart) { # loop over parts
+      twowrists = which(ses == sesi & id ==idi & wrist == TRUE & part == parti)
+      if (length(twowrists) == 2) { # if a pair is found load the data
+        loc1 = loc[twowrists[1]]
+        if (loc1 == "nondomwrist") {
+          nondomwrist = read.csv(fnamesfull[twowrists[1]])
+          domwrist = read.csv(fnamesfull[twowrists[2]])
+        } else {
+          nondomwrist = read.csv(fnamesfull[twowrists[2]])
+          domwrist = read.csv(fnamesfull[twowrists[1]])
+        }
+        # fuse the data from both wrists
+        S = merge(nondomwrist,domwrist,by=c("numerictime"),suffixes = c(".nd",".dm"),all = TRUE) #nd =nondominant, dm=dominant
+        S$time.nd = as.POSIXlt(S$time.nd)
+        S$time.dm = as.POSIXlt(S$time.dm)
+        if (do.plot == TRUE) {
+          # Create plot of the timeseries and the matching handclaps in red:
+          x11();
+          matchingpeaks = which(S$acc_peak.nd == 1 & S$acc_peak.dm== 1)
+          
+          par(mfrow=c(2,1))
+          plot(S$time.nd,S$acc_mean.nd,type="l",main=paste0("nondominant wrist ",idi," ",sesi),
+               ylim=c(0,5),ylab="acceleration (g)",xlab="time")
+          lines(S$time.nd,S$acc_max.nd,type="l",col="blue")
+          lines(S$time.nd[matchingpeaks],rep(1,length(matchingpeaks)),type="p",col="red",pch=20)
+          plot(S$time.dm,S$acc_mean.dm,type="l",main=paste0("dominant wrist ",idi," ",sesi),
+               ylim=c(0,5),ylab="acceleration (g)",xlab="time")
+          lines(S$time.dm,S$acc_max.dm,type="l",col="blue")
+          lines(S$time.dm[matchingpeaks],rep(1,length(matchingpeaks)),type="p",col="red",pch=20)
+          # Write matching handclap timestamps to a csv file:
+          peaks = data.frame(timestamp=S$time.nd[matchingpeaks])
+          print(paste0("Number of handclaps: ",nrow(peaks)))
+          write.csv(peaks,file=paste0(handclapfolder,"/PossibleHandClapTimes_",idi,"_",sesi,".csv"),row.names = FALSE)
+        }
       }
-      # fuse the data from both wrists
-      S = merge(nondomwrist,domwrist,by=c("numtime"),suffixes = c(".nd",".dm"),all = TRUE)
-      S$time.nd = as.POSIXlt(S$time.nd)
-      S$time.dm = as.POSIXlt(S$time.dm)
-      # Create plot of the timeseries and the matching handclaps in red:
-      x11();
-      matchingpeaks = which(S$accWR_peak.nd == 1 & S$accWR_peak.dm== 1)
-      par(mfrow=c(2,1))
-      plot(S$time.nd,S$accWR_mean.nd,type="l",main=paste0("nondominant wrist ",idi," ",sesi),
-           ylim=c(0,5),ylab="acceleration (g)",xlab="time")
-      lines(S$time.nd,S$accWR_max.nd,type="l",col="blue")
-      lines(S$time.nd[matchingpeaks],rep(1,length(matchingpeaks)),type="p",col="red",pch=20)
-      plot(S$time.dm,S$accWR_mean.dm,type="l",main=paste0("dominant wrist ",idi," ",sesi),
-           ylim=c(0,5),ylab="acceleration (g)",xlab="time")
-      lines(S$time.dm,S$accWR_max.dm,type="l",col="blue")
-      lines(S$time.dm[matchingpeaks],rep(1,length(matchingpeaks)),type="p",col="red",pch=20)
-      # Write matching handclap timestamps to a csv file:
-      peaks = data.frame(timestamp=S$time.nd[matchingpeaks])
-      write.csv(peaks,file=paste0(handclapfolder,"/PossibleHandClapTimes_",idi,"_",sesi,".csv"),row.names = FALSE)
     }
   }
 }
--- extractfeatures.R
+++ extractfeatures.R
@@ -1,21 +1,25 @@
-# Data exploration Shimmer device - 20 February 2019 (by: Vincent van Hees)
+# Data exploration Shimmer device - February 2019 (by: Vincent van Hees)
 rm(list=ls()) # WARNING: this will empty your workspace, comment this out if you do not want that
 graphics.off() # WARNING: this will close all open figures, comment this out if you do not want that
-#==============================================
+#=============================================================
 # Key input parameters from user:
-#==============================================
+#=============================================================
 # specify location of data folder and output folder:
 path = "/media/sf_sharedfolder/Emotion/accelerometer_data"
 datafolder = paste0(path,"/datamichel")
 outputfolder = paste0(path,"/myresults")
 
-# Epoch size in seconds to which data will be aggregated:
-epochsize = 1
-# Whether or not to plot some data on screen (mainly useful for testing purposes):
-do.plot = TRUE # change to FALSE to turn off / change to TRUE to turn on
+AccelerometerType = "WR" # Choose which of the two accelerometers to use, alternatively use "LN"
+DeviceSerialNumbers_dominantwrist = c("CD5D", "C9BB")
+DeviceSerialNumbers_nondominantwrist = c("D910", "D821")
+DeviceSerialNumbers_chest = c("D977", "DA9F")
+epochsize = 1 # Epoch size in seconds to which data will be aggregated:
+do.plot = TRUE # Create plot on screen (for testing). change to FALSE to turn off / change to TRUE to turn on
 
+#----------------------------------------------------------------------
 # Abbreviations:
-# - WR:  Wide range accelerometer (see Shimmer documentation), we are not using the Low noise accelerometer now.
+# - WR:  Wide range accelerometer (see Shimmer documentation)
+# - LN:  Low noise accelerometer now  (see Shimmer documentation)
 # - Yaw, roll and pitch: Euler angles provided by Shimmer software (see Shimmer documention)
 # - enmo: refers to metric used for calculating magnitude of acceleration (abbreviation only
 #         used in code, not in variable names.
@@ -25,7 +29,7 @@ do.plot = TRUE # change to FALSE to turn off / change to TRUE to turn on
 # Units:
 # - peak variables are indicated by 1 (peak) 0 (not a peak).
 # - all other variables are in m/s2, unless otherwise indicated.
-#====================================================
+#=============================================================
 # call packages and declare functions
 list.of.packages <- c("signal", "data.table")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -39,11 +43,11 @@ get_enmo =  function(x,y,z) {
 p50 = function(x) return(quantile(x,probs = 0.50))
 p75 = function(x) return(quantile(x,probs = 0.75))
 p99 = function(x) return(quantile(x,probs = 0.99))
-addvarEnmoWR = function(x,varname = "") {
+addvarEnmo = function(x,varname = "") {
   # replace variable "enmo" with a new name
   # this is needed for the output of the aggregate function
-  x[varname] = x$enmo_WR
-  x = x[,-which(colnames(x) == "enmo_WR")]
+  x[varname] = x$enmo
+  x = x[,-which(colnames(x) == "enmo")]
   return(x)
 }
 options(digits.secs=12) # to get more precise fractions of seconds
@@ -107,13 +111,14 @@ for (fi in 1:length(fnames)) {
           }
         }
         # derive attachment location from device serial number
-        if (sn == "CD5D" | sn == "C9BB") {
+        if (sn %in% DeviceSerialNumbers_dominantwrist) {
           bodyside = "domwrist"
-        } else if (sn == "D910" | sn == "D821") {
+        } else if (sn %in% DeviceSerialNumbers_nondominantwrist) {
           bodyside = "nondomwrist"
-        } else if (sn == "D977" | sn == "DA9F") {
+        } else if (sn %in% DeviceSerialNumbers_chest) {
           bodyside = "chest"
         }
+        
         #remove all device identifiers from varnames, to allow for more generic code
         for (jj in 1:length(varname)) {
           partname = unlist(strsplit(varname[jj],"_"))[2] #assumption that name is structured as letters_letters_variablename
@@ -139,13 +144,17 @@ for (fi in 1:length(fnames)) {
         if (do.call ==  FALSE) { # if we do not want to do calibration then focus on feature extraction
           #-------------------------------
           # calculate enmo features (magnitude of acceleration)
-          enmo_WR = get_enmo(x=D$Accel_WR_X_CAL,y=D$Accel_WR_Y_CAL,z=D$Accel_WR_Z_CAL)
+          if (AccelerometerType == "WR") {
+            enmo = get_enmo(x=D$Accel_WR_X_CAL,y=D$Accel_WR_Y_CAL,z=D$Accel_WR_Z_CAL)
+          } else {
+            enmo = get_enmo(x=D$Accel_LN_X_CAL,y=D$Accel_LN_Y_CAL,z=D$Accel_LN_Z_CAL)
+          }
           # remove low frequency component, probably related to imperfect calibration
           lb = 0.2 # lower boundary of the filter
           n = 4 # filter order
           bf = signal::butter(n,c(lb/(sf/2)),type=c("high")) #creating filter coefficients
           # also ignore direction of acceleration now signal is high pass filtered
-          enmo_WR = abs(signal::filter(bf,enmo_WR))
+          enmo = abs(signal::filter(bf,enmo))
           # Calculate separate enmo specifically for peak detecion only for WR for now
           lb = 5 # lower boundary of the filter
           hb = 50 # higher bounder of the filter
@@ -153,13 +162,15 @@ for (fi in 1:length(fnames)) {
           Wc[1,1] = lb / (sf/2)
           Wc[2,1] = hb / (sf/2)
           bf = signal::butter(n,Wc,type=c("pass"))
-          WRX = D$Accel_WR_X_CAL
-          WRY = D$Accel_WR_Y_CAL
-          WRZ = D$Accel_WR_Z_CAL
-          WRX = abs(signal::filter(bf,WRX))
-          WRY = abs(signal::filter(bf,WRY))
-          WRZ = abs(signal::filter(bf,WRZ))
-          enmopeak = get_enmo(x=WRX,y=WRY,z=WRZ)
+          if (AccelerometerType == "WR") {
+            GX = D$Accel_WR_X_CAL; GY = D$Accel_WR_Y_CAL; GZ = D$Accel_WR_Z_CAL
+          } else {
+            GX = D$Accel_LN_X_CAL; GY = D$Accel_LN_Y_CAL; GZ = D$Accel_LN_Z_CAL
+          }
+          GX = abs(signal::filter(bf,GX))
+          GY = abs(signal::filter(bf,GY))
+          GZ = abs(signal::filter(bf,GZ))
+          enmopeak = get_enmo(x=GX,y=GY,z=GZ)
         }
         # downsample
         if (epochsize < 1) {
@@ -170,14 +181,27 @@ for (fi in 1:length(fnames)) {
                                * epochsize)
         }
         if (do.call ==  FALSE) {
-          df_kin = data.frame(enmo_WR=enmo_WR,#enmo_LN=enmo_LN,
-                              pitch_WR = D$Euler_9DOF_Pitch_WR_CAL,
-                              roll_WR = D$Euler_9DOF_Roll_WR_CAL,
-                              yaw_WR = D$Euler_9DOF_Yaw_WR_CAL,enmopeak = enmopeak,
-                              time=D$timestamp,numtime=FiveSecIndex)
+          if (AccelerometerType == "WR") {
+            df_kin = data.frame(enmo=enmo,
+                                pitch = D$Euler_9DOF_Pitch_WR_CAL,
+                                roll = D$Euler_9DOF_Roll_WR_CAL,
+                                yaw = D$Euler_9DOF_Yaw_WR_CAL,enmopeak = enmopeak,
+                                time=D$timestamp,numerictime=FiveSecIndex)
+          } else {
+            df_kin = data.frame(enmo=enmo,
+                                pitch = D$Euler_9DOF_Pitch_LN_CAL,
+                                roll = D$Euler_9DOF_Roll_LN_CAL,
+                                yaw = D$Euler_9DOF_Yaw_LN_CAL,enmopeak = enmopeak,
+                                time=D$timestamp,numerictime=FiveSecIndex)
+          }
         } else { # variables needed for accelerometer calibration assessment
-          df_kin = data.frame(Accel_WR_X = D$Accel_WR_X_CAL, Accel_WR_Y = D$Accel_WR_Y_CAL, Accel_WR_Z = D$Accel_WR_Z_CAL,
-                               time=D$timestamp,numtime=FiveSecIndex)
+          if (AccelerometerType == "WR") {
+            df_kin = data.frame(Accel_X = D$Accel_WR_X_CAL, Accel_Y = D$Accel_WR_Y_CAL, Accel_Z = D$Accel_WR_Z_CAL,
+                                time=D$timestamp,numerictime=FiveSecIndex)
+          } else {
+            df_kin = data.frame(Accel_X = D$Accel_LN_X_CAL, Accel_Y = D$Accel_LN_Y_CAL, Accel_Z = D$Accel_LN_Z_CAL,
+                                time=D$timestamp,numerictime=FiveSecIndex)
+          }
         }
         if ("GSR_Skin_Conductance_CAL" %in% varname & do.call ==  FALSE) {
           do.skinsensors = TRUE  
@@ -187,52 +211,52 @@ for (fi in 1:length(fnames)) {
         if (do.skinsensors == TRUE) {
           df_skin = data.frame(GSR_Skin_Conductance = D$GSR_Skin_Conductance_CAL,
                                GSR_Skin_Resistance = D$GSR_Skin_Resistance_CAL,
-                               time=D$timestamp, numtime=FiveSecIndex)
+                               time=D$timestamp, numerictime=FiveSecIndex)
           # ignore first half epoch to only get full epochs
-          firstfullepoch = unique(df_skin$numtime)[2]
-          df_skin = df_skin[which(df_skin$numtime >= firstfullepoch),]
+          firstfullepoch = unique(df_skin$numerictime)[2]
+          df_skin = df_skin[which(df_skin$numerictime >= firstfullepoch),]
           # aggregate
-          Oskin = aggregate(x = df_skin,by = list(df_skin$numtime),mean)
+          Oskin = aggregate(x = df_skin,by = list(df_skin$numerictime),mean)
         }
         # ignore first half epoch to only get full epochs
-        firstfullepoch = unique(df_kin$numtime)[2]
-        df_kin = df_kin[which(df_kin$numtime >= firstfullepoch),]
+        firstfullepoch = unique(df_kin$numerictime)[2]
+        df_kin = df_kin[which(df_kin$numerictime >= firstfullepoch),]
         # aggregate
-        Omean = aggregate(x = df_kin,by = list(df_kin$numtime),mean)
-        Ostd = aggregate(x = df_kin[,c("enmo_WR","numtime")],by = list(df_kin$numtime),sd)
-        # Ostd = aggregate(x = df_kin[,c("enmo_WR","enmo_LN","numtime")],by = list(df_kin$numtime),sd)
+        Omean = aggregate(x = df_kin,by = list(df_kin$numerictime),mean)
+        Ostd = aggregate(x = df_kin[,c("enmo","numerictime")],by = list(df_kin$numerictime),sd)
         if (do.call ==  FALSE) { # additional aggregations if calibration is not done
-          O50 = aggregate(x = df_kin[,c("enmopeak","numtime","time")],by = list(df_kin$numtime),p50)
-          O75 = aggregate(x = df_kin[,c("enmopeak","numtime")],by = list(df_kin$numtime),p75)
-          O99 = aggregate(x = df_kin[,c("enmopeak","numtime")],by = list(df_kin$numtime),p99)
-          Omax = aggregate(x = df_kin[,c("enmopeak","numtime")],by = list(df_kin$numtime),max)
+          O50 = aggregate(x = df_kin[,c("enmopeak","numerictime","time")], by = list(df_kin$numerictime),p50)
+          O75 = aggregate(x = df_kin[,c("enmopeak","numerictime")], by = list(df_kin$numerictime),p75)
+          O99 = aggregate(x = df_kin[,c("enmopeak","numerictime")], by = list(df_kin$numerictime),p99)
+          Omax = aggregate(x = df_kin[,c("enmopeak","numerictime")], by = list(df_kin$numerictime),max)
           # update variable names
-          Omean = addvarEnmoWR(x = Omean, varname="enmoWR_mean")
-          Ostd = addvarEnmoWR(x = Ostd, varname="enmoWR_std")
+          Omean = addvarEnmo(x = Omean, varname="enmo_mean")
+          Ostd = addvarEnmo(x = Ostd, varname="enmo_std")
           # Now put all relevant aggregated variables in a dataframe:
           agData = data.frame(time=O50$time,
-                              numtime=O50$numtime,
-                              accWR_percentile50=O50$enmopeak,
-                              accWR_percentile75=O75$enmopeak,
-                              accWR_percentile99=O99$enmopeak,
-                              accWR_max=Omax$enmopeak,
-                              accWR_mean=Omean$enmoWR_mean,
-                              accWR_std=Ostd$enmoWR_std,
-                              pitch_WR_mean = Omean$pitch_WR,
-                              roll_WR_mean = Omean$roll_WR,
-                              yaw_WR_mean = Omean$yaw_WR)
+                              secondsinrecording=O50$numerictime-O50$numerictime[1],
+                              numerictime=O50$numerictime,
+                              acc_percentile50=O50$enmopeak,
+                              acc_percentile75=O75$enmopeak,
+                              acc_percentile99=O99$enmopeak,
+                              acc_max=Omax$enmopeak,
+                              acc_mean=Omean$enmo_mean,
+                              acc_std=Ostd$enmo_std,
+                              pitch_mean = Omean$pitch,
+                              roll_mean = Omean$roll,
+                              yaw_mean = Omean$yaw)
           # identify peaks in the magnitude of acceleration (enmo)
-          agData$accWR_peak = 0
+          agData$acc_peak = 0
           # peaks are defined here as:
           # (maximum values in an epoch at least 200% the 99th percentile of that epoch
           # AND with a value above 2 m/s2) OR with max acceleration above 10
-          peakindexWR = which(((agData$accWR_max-agData$accWR_percentile50) > 
-                                 ((agData$accWR_percentile99-agData$accWR_percentile50)*2) & agData$accWR_max > 2) |
-                                agData$accWR_max > 5)
-          agData$accWR_peak[peakindexWR] = 1 
+          peakindex = which(((agData$acc_max-agData$acc_percentile50) > 
+                               ((agData$acc_percentile99-agData$acc_percentile50)*2) & agData$acc_max > 2) |
+                              agData$acc_max > 5)
+          agData$acc_peak[peakindex] = 1 
         } else { # when investigating autocalibration the output variables are simpler.
           agData = data.frame(time=Omean$time,
-                              numtime=Omean$numtime,
+                              numerictime=Omean$numerictime,
                               Accel_LN_X_mean = Omean$Accel_LN_X,
                               Accel_LN_Y_mean = Omean$Accel_LN_Y,
                               Accel_LN_Z_mean = Omean$Accel_LN_Z,
@@ -253,10 +277,10 @@ for (fi in 1:length(fnames)) {
         # plot low resolution data on screen:
         if (do.plot == TRUE & do.call == FALSE) {
           x11()
-          plot(agData$time,agData$accWR_mean,type="l",ylim=c(0,max(agData$accWR_max)),main="accWR",
+          plot(agData$time,agData$acc_mean,type="l",ylim=c(0,max(agData$acc_max)),main="acc",
                col="blue",xlab="timestamp",ylab="acceleration (m/s2)")
-          lines(agData$time,agData$accWR_max,type="l",col="green")
-          lines(agData$time[peakindexWR],agData$accWR_max[peakindexWR],col="red",type="p",pch=20,cex=0.5)
+          lines(agData$time,agData$acc_max,type="l",col="green")
+          lines(agData$time[peakindex],agData$acc_max[peakindex],col="red",type="p",pch=20,cex=0.5)
           legend("topleft",legend = c("mean","max"),col=c("blue","green"),lty=c(1,1))
         }
         if (length(data2store) == 0) {
@@ -268,9 +292,15 @@ for (fi in 1:length(fnames)) {
     }
     graphics.off()
   }
-  print(sfstore)
   sfmean = mean(sfstore)
-  write.csv(data2store,file = paste0(outputfolder,"/shim_aggre_call",as.character(do.call),"_",bodyside,"_",fnameshort_withoutext,"_sf",sfmean,".csv"),row.names = FALSE)
+  if (do.call == FALSE) {
+    write.csv(data2store,file = paste0(outputfolder,"/shimmer_",
+                                       bodyside,"_",fnameshort_withoutext,"_sf",sfmean,
+                                       "_acc",AccelerometerType,".csv"),row.names = FALSE)
+  } else {
+    write.csv(data2store,file = paste0(outputfolder,"/shim_calibrationcheck_",
+                                       fnameshort_withoutext,".csv"),row.names = FALSE)
+  }
   timer1 = Sys.time()
   cat("\nTime elapsed:")
   print(timer1-timer0)
